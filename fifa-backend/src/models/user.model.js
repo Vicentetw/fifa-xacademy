c82c7d3eafde75e02fbefd99c0786a4eac506d31
Vicentetw <perrottavicente@gmail.com>
2026-04-23 09:54:25 -0300

    Creo user.moldel.js

---
+++ fifa-backend/src/models/user.model.js
@@ -0,0 +1,15 @@
+const { db } = require('../config/database');
+
+const createUserTable = async () => {
+    await db.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+};
+module.exports = {
+    createUserTable
+};
