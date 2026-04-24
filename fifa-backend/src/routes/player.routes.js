645209dfb2f97f94cc354b0cead201433d063999
Vicentetw <perrottavicente@gmail.com>
2026-04-24 09:20:29 -0300

    agrego route de player

--- fifa-backend/src/routes/player.routes.js
+++ fifa-backend/src/routes/player.routes.js
@@ -2,13 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const authMiddleware = require('../middlewares/auth.middleware');
+const { createPlayer, getAllPlayers, getPlayerById, updatePlayer, deletePlayer } = require('../controllers/player.controller');
 
-console.log('Middleware:', authMiddleware);
+//console.log('Middleware:', authMiddleware);
+
+// Rutas CRUD para jugadores
+
+router.post('/', authMiddleware, createPlayer);
+//router.get('/', authMiddleware, getAllPlayers);
+//router.get('/:id', authMiddleware, getPlayerById);
+//router.put('/:id', authMiddleware, updatePlayer);
+//router.delete('/:id', authMiddleware, deletePlayer);
 
 // Ruta protegida
 router.get('/', authMiddleware, (req, res) => {
   res.json({
-    message: 'Accediste a jugadores',
+    message: 'Accediste a jugadores, test de ruta protegida',
     user: req.user
   });
 });
