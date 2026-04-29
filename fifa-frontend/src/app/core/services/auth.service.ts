3fdc04e92cc6dd3cd0475fa91b97cacf32fc0b92
Vicentetw <perrottavicente@gmail.com>
2026-04-29 10:53:58 -0300

    auth service creo currentUser, para guardar user, ontengo, lo seteo, y lo obtengo para no hacer peticiones de usuario

--- fifa-frontend/src/app/core/services/auth.service.ts
+++ fifa-frontend/src/app/core/services/auth.service.ts
@@ -10,6 +10,9 @@ export class AuthService {
 
   private API_URL = 'http://localhost:3000/api/auth';
 
+    // guardo el usuario en memoria después lo obtengo con getMe() para no hacer una petición cada vez que quiera saber quién es el usuario logeado
+  private currentUser: any = null;
+
   constructor(private http: HttpClient) {}
 
   // 🔐 LOGIN
@@ -21,12 +24,21 @@ export class AuthService {
     );
   }
 
-  // 👇 AGREGAR JUSTO ACÁ (debajo de login)
+  // obtener usuario logeado desde backend (debajo de login)
   getMe(): Observable<any> {
     return this.http.get(
       `${this.API_URL}/me`,
       { withCredentials: true }
     );
+  }
+
+// guardo usuario en memoria
+  setUser(user: any) {
+    this.currentUser = user;
+  }
+  // obtengo usuario desde memoria
+  getUser() {
+    return this.currentUser;
   }
 
   logout(): Observable<any> {
