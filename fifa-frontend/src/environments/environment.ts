3e68d72483adb2d44e49b0ba316dc1dde92c9d2d
Vicentetw <perrottavicente@gmail.com>
2026-05-19 08:56:08 -0300

    modifico environment e importación en variables en players.service

--- fifa-frontend/src/environments/environment.ts
+++ fifa-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
-  /*apiUrl: 'http://localhost:3000/api'*/
-  apiUrl: 'https://fifa-xacademy.onrender.com/api'
+  production: false,
+  apiUrl: 'http://localhost:3000/api'
 };
